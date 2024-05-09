83d83f62643424ac2923e6487ebff3ab751d2fda
Ana Chicaiza <anachicaiza@icloud.com>
2024-05-09 15:33:59 +0100

    JS updated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
-function getWeather(city) {
-  let apiKey = "b739b64actfb7710ab2aa8f6044o4c38";
-  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  console.log(apiUrl);
+function change(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#description");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+  descriptionElement.innerHTML = response.data.condition.description;
 }
 
-function citySearch(event) {
+function search(event) {
   event.preventDefault();
   let searchElement = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchElement.value;
-}
+  let city = searchElement.value;
 
-let searchElement = document.querySelector("#search-form");
-searchElement.addEventListener("submit", citySearch);
+  let apiKey = "b739b64actfb7710ab2aa8f6044o4c38";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(change);
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+let currentDateInput = document.querySelector("#current-date");
+let currentDate = new Date();
+currentDateInput.innerHTML = formatDate(currentDate);
