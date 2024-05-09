c4e92c302f64a6637479212c7be0f0b144993f6f
Ana Chicaiza <anachicaiza@icloud.com>
2024-05-09 01:51:14 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+function getWeather(city) {
+    let apiKey = "b739b64actfb7710ab2aa8f6044o4c38"
+    let apiURL = "https://api.shecodes.io/weather/v1/current?query={query}&key={key}"
+}
+
+
+
+
+
 function citySearch(event) {
   event.preventDefault();
   let searchElement = document.querySelector("#search-input");
   let cityElement = document.querySelector("#current-city");
   cityElement.innerHTML = searchElement.value;
 }
+
 let searchElement = document.querySelector("#search-form");
 searchElement.addEventListener("submit", citySearch);
